137945b6b914138b5389928eeeb3fb005931a514
Jonathan Brinley <jbrinley@flightless.us>
2019-07-31 15:46:41 -0400

    override pricing api instead of rest controller

---
+++ src/BigCommerce_Price_Cache/Api/Cached_Pricing_Api.php
@@ -0,0 +1,102 @@
+<?php
+
+
+namespace BigCommerce_Price_Cache\Api;
+
+use BigCommerce\Api\v3\Api\PricingApi;
+use BigCommerce\Api\v3\ApiException;
+use BigCommerce\Api\v3\Model\ItemPricing;
+use BigCommerce\Api\v3\Model\Meta;
+use BigCommerce\Api\v3\Model\PricingRequest;
+use BigCommerce\Api\v3\Model\PricingRequestItem;
+use BigCommerce\Api\v3\Model\PricingResponse;
+use BigCommerce\Api\v3\ObjectSerializer;
+use BigCommerce\Post_Types\Product\Product;
+use BigCommerce\Taxonomies\Channel\Channel;
+use BigCommerce_Price_Cache\Exceptions\Cache_Not_Found_Exception;
+use BigCommerce_Price_Cache\Import\Processors\Fetch_Price_Cache;
+
+/**
+ * Class Cached_Pricing_Api
+ *
+ * Intercepts requests to the pricing API, returning data from
+ * WordPress post meta if the request is for default item states
+ */
+class Cached_Pricing_Api extends PricingApi {
+	/**
+	 * Operation getPrices
+	 * Get Prices
+	 *
+	 *
+	 * @param PricingRequest $body   (required)
+	 * @param array          $params = []
+	 *
+	 * @return PricingResponse
+	 * @throws ApiException on non-2xx response
+	 * @throws \InvalidArgumentException
+	 */
+	public function getPrices( $body, array $params = [] ) {
+		if ( $body->getCustomerGroupId() ) {
+			// only the default customer group gets cached prices
+			return parent::getPrices( $body, $params );
+		}
+
+		$items = $body->getItems();
+
+		if ( $this->has_uncacheable_items( $items ) ) {
+			return parent::getPrices( $body, $params );
+		}
+
+		try {
+			$channel_id = $body->getChannelId();
+			$channel    = Channel::find_by_id( $channel_id );
+
+			$items = array_map( function ( $item ) use ( $channel ) {
+				return $this->get_cached_pricing( $item, $channel );
+			}, $items );
+
+			return new PricingResponse( [
+				'data' => $items,
+				'meta' => new Meta(),
+			] );
+		} catch ( \Exception $e ) {
+			return parent::getPrices( $body, $params );
+		}
+	}
+
+	/**
+	 * @param PricingRequestItem[]|array[] $items
+	 *
+	 * @return bool
+	 */
+	private function has_uncacheable_items( $items ) {
+		foreach ( $items as $item ) {
+			if ( ! empty( $item['options'] ) ) {
+				return true;
+			}
+			if ( ! empty( $item['variant_id'] ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param PricingRequestItem|array $item
+	 * @param \WP_Term                 $channel
+	 *
+	 * @return array|object|null
+	 */
+	private function get_cached_pricing( $item, \WP_Term $channel ) {
+		$product = Product::by_product_id( $item['product_id'], $channel );
+		$meta    = get_post_meta( $product->post_id(), Fetch_Price_Cache::CACHE_PREFIX . $channel->term_id, true );
+		if ( empty( $meta ) ) {
+			throw new Cache_Not_Found_Exception( __( 'No pricing cache found for product ID ' . $item['product_id'] ) );
+		}
+
+		/** @var ItemPricing $item */
+		return ObjectSerializer::deserialize( json_decode( $meta ), ItemPricing::class );
+	}
+
+}
--- src/BigCommerce_Price_Cache/Container/Rest_Provider.php
+++ src/BigCommerce_Price_Cache/Container/Rest_Provider.php
@@ -6,14 +6,21 @@ namespace BigCommerce_Price_Cache\Container;
 use BigCommerce\Container\Api;
 use BigCommerce\Container\Provider;
 use BigCommerce\Container\Rest;
-use BigCommerce_Price_Cache\Rest\Cached_Pricing_Controller;
+use BigCommerce\Rest\Pricing_Controller;
+use BigCommerce_Price_Cache\Api\Cached_Pricing_Api;
 use Pimple\Container;
 
 class Rest_Provider extends Provider {
+	const CACHED_PRICING_API = 'bigcommerce_price_cache.rest.cached_pricing_api';
+
 	public function register( Container $container ) {
-		// Override the pricing controller
+		$container[ self::CACHED_PRICING_API ] = function( Container $container ) {
+			return new Cached_Pricing_Api( $container[ Api::CLIENT ] );
+		};
+
+		// Override the pricing controller to use our caching API
 		$container[ Rest::PRICING ] = function( Container $container ) {
-			return new Cached_Pricing_Controller( $container[ Rest::NAMESPACE_BASE ], $container[ Rest::VERSION ], $container[ Rest::PRICING_BASE ], $container[ Api::FACTORY ]->pricing() );
+			return new Pricing_Controller( $container[ Rest::NAMESPACE_BASE ], $container[ Rest::VERSION ], $container[ Rest::PRICING_BASE ], $container[ self::CACHED_PRICING_API ] );
 		};
 	}
 }
