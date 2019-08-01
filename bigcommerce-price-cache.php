22e65619e83559c14a222619ab9e41d63c2c7ea7
Jonathan Brinley <jbrinley@flightless.us>
2019-08-01 10:21:52 -0400

    don't init if bigcommerce version is less than 3.5

--- bigcommerce-price-cache.php
+++ bigcommerce-price-cache.php
@@ -14,8 +14,6 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
 
-register_activation_hook( __FILE__, [ \BigCommerce\Plugin::class, 'activate' ] );
-
 // Start the plugin
 add_action( 'bigcommerce/init', 'bigcommerce_price_cache_init', 1, 2 );
 
@@ -26,6 +24,9 @@ add_action( 'bigcommerce/init', 'bigcommerce_price_cache_init', 1, 2 );
  * @param \Pimple\Container   $container   The DI container for the BigCommerce plugin
  */
 function bigcommerce_price_cache_init( \BigCommerce\Plugin $bigcommerce, \Pimple\Container $container ) {
+	if ( version_compare( $bigcommerce::VERSION, '3.5', '<' ) ) {
+		return;
+	}
 	$plugin = \BigCommerce_Price_Cache\Plugin::instance( $container );
 	$plugin->init();
 }
